eed2860f005f4478fe47be8aa85f6d162523fa4c
P1ST-0N <luckylionia@gmail.com>
2024-06-08 19:35:23 +0300

    Create authRouter.js

---
+++ routes/authRouter.js
@@ -0,0 +1,40 @@
+import express from "express";
+// import authCtrl
+import userSchemas from "../schemas/usersSchemas.js";
+import validateBody from "../helpers/validateBody.js";
+import authControl from "../middlewares/authControl";
+import uploadAvatar from "../middlewares/uploadAvatar.js";
+
+const usersRouter = express.Router();
+
+usersRouter.post("/register", validateBody(userSchemas.create, true)); //authCtrl.register
+
+usersRouter.post("/login", validateBody(userSchemas.create, true)); //login
+
+usersRouter.post("/logout", authControl); //logout
+
+usersRouter.get("/current", authControl); //current
+
+usersRouter.patch(
+  "/",
+  authControl,
+  validateBody(userSchemas.updateSubscription)
+  //updateSubscription
+);
+
+usersRouter.patch(
+  "/avatars",
+  authControl,
+  uploadAvatar.single("avatar")
+  //updateAvatar
+);
+
+usersRouter.get("/verify/:verificationToken"); //authCtrl.verifyEmail
+
+usersRouter.post(
+  "/verify",
+  validateBody(userSchemas.verifyEmail, true)
+  //sendVerificationEmail
+);
+
+export default usersRouter;
