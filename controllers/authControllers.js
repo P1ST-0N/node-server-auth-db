6c2e333a677af8e859912dde2a46d7382029aae3
P1ST-0N <luckylionia@gmail.com>
2024-06-11 13:27:18 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -29,4 +29,42 @@ const register = async (req, res, next) => {
   });
 
   //   await mailService.sendEmail
+
+  res.status(201).json({ user: { email, subscription } });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const errorMessage = "Email or password is wrong";
+
+  const user = await usersService.get({ email });
+
+  if (!user) throw HttpError(401, errorMessage);
+
+  if (!user.verify) throw HttpError(401, "Email is not verified");
+
+  const isCorrectPass = await bcrypt.compare(password, user.password);
+
+  if (!isCorrectPass) throw HttpError(401, errorMessage);
+
+  const token = jwt.sign({ id: user._id }, process.env.DB_SECRET, {
+    expiresIn: "23h",
+  });
+
+  await usersService.update({ id: user._id, token });
+
+  const resBody = {
+    token,
+    user: {
+      email,
+      subscription: user.subscription,
+    },
+  };
+
+  res.json(resBody);
+};
+
+export default {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
